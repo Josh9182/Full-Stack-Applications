71fa7a7eeb5fc60b4a1d42f3fa053978d5c9d9d8
Josh <xxwhatup2xx@gmail.com>
2025-08-31 15:18:59 -0600

    Added invalid input test to handle null files, but discarded it. Will design controller with null handler

--- FileConverter/Backend/FileConverter.Backend.Tests/ServiceTests/ParserTests/CsvFileReaderTest.cs
+++ FileConverter/Backend/FileConverter.Backend.Tests/ServiceTests/ParserTests/CsvFileReaderTest.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 
 namespace ServiceTests.ParserTests;
+
 public class CsvFileReaderTests
 {
     [Fact]
---
+++ FileConverter/Backend/FileConverter.Backend.Tests/ServiceTests/ParserTests/TxtFileReaderTest.cs
@@ -0,0 +1,26 @@
+using FileConverter.Interfaces;
+using Services.Readers;
+using System.Text;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace ServiceTests.ParserTests;
+public class TxtFileReaderTests
+{
+    [Fact]
+    [Trait("Category", "TxtFRT_VI")]
+    public async Task CsvFRT_ValidInput()
+    {
+        var content = "Hello!\nGoodbye!\nHello!";
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+        var csvReader = new CsvFileReader();
+
+        var result = await csvReader.Reader(stream);
+
+        var resultCount = result.Sum(d => d.Count);
+
+        Assert.Equal(2, resultCount); // Count of records within the Task<List<Dictionary<string, object>>>
+        Assert.NotNull(result); // CSV successfully parsed
+    }
+}

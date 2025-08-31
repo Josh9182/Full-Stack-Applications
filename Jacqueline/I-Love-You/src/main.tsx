1bf85a9eb16a43406023c484963a5e5b82520445
Josh Lewis <xxwhatup2xx@gmail.com>
2025-08-30 22:18:54 -0600

    Delete Jacqueline/I-Love-You directory

--- Jacqueline/I-Love-You/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ILoveYouDarling from './ILoveYouDarling'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ILoveYouDarling />
-  </StrictMode>,
-)

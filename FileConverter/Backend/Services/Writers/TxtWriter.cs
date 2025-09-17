fe0b66f6dc24d63c963b03bab314d9495a4f17c6
Josh <xxwhatup2xx@gmail.com>
2025-09-16 22:07:56 -0600

    FlattenDict helper method completed, changes nested dict to mainKey.nestedKey... infinitely. It can handle dictionary's, lists, and primitives

--- FileConverter/Backend/Services/Writers/TxtWriter.cs
+++ FileConverter/Backend/Services/Writers/TxtWriter.cs
@@ -13,12 +13,32 @@ public class TxtFileWriter : IFileWriter
 
         foreach (var dict in records)
         {
-            string nameKey = string.IsNullOrEmpty(parentKey) ? kvp.Key : $"{parentKey}.{kvp.Key}";
+            string nameKey = string.IsNullOrEmpty(parentKey) ? dict.Key : $"{parentKey}.{dict.Key}"; // format to store parent.child... : value
 
-            if (kvp.Value is Dictionary<string, object> nestedDict)
+            if (dict.Value is Dictionary<string, object> nestedDict) // Checking if each dictionary has a nested dictionary as a value
             {
-                flattenedDictResult.AddRange(FlattenDict(nestedDict, parentKey)); 
+                flattenedDictResult.AddRange(FlattenDict(nestedDict, nameKey)); // Recursive call accessing the nestedDict with the new nameKey
             }
-        }   
+            else if (dict.Value is List<object> list) // Checking if each dictionary has a nested list as a value
+            {
+                for (int i = 0; i < list.Count; i++) // for loop, storing index in case of duplicate keys for parentKey.childKey
+                {
+                    var item = list[i]; // Each element gets accounted for 
+                    if (item is Dictionary<string, object> listDict)
+                    {
+                        flattenedDictResult.AddRange(FlattenDict(listDict, nameKey)); // Recursive call 
+                    }
+                    else // if primitive...
+                    {
+                        flattenedDictResult.Add($"\"{nameKey}[{i}]\": \"{item}\""); // Add straight to the list of strings with format "nameKey[i]": "item"
+                    }
+                }
+            }
+            else // If non-nested value == primitive
+            { 
+                flattenedDictResult.Add($"\"{nameKey}\": \"{dict.Value}\"");
+            }
+        }
+        return flattenedDictResult;
     }
 }

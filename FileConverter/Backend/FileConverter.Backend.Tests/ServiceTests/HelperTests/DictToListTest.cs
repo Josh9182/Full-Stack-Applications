62a8772e9f367a3016cb9a87acddf444eb749554
Josh <xxwhatup2xx@gmail.com>
2025-09-20 00:40:30 -0600

    Test structure added for DictToList

---
+++ FileConverter/Backend/FileConverter.Backend.Tests/ServiceTests/HelperTests/DictToListTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using FileConverter.Interfaces;
+using Services.Helpers;
+
+namespace ServiceTests.HelperTests;
+
+public class DictToListTests
+{
+    [Fact]
+    [Trait("Category", "DTL_VI")]
+    public async Task DictToList_ValidInput()
+    {
+
+    }
+}
